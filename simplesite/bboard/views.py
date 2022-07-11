0a202f38b2bd1bc9fbd30a538b9f6d2e968acab0
Andriyevski <andriyevski@gmail.com>
2022-07-12 01:06:09 +0300

    Today i add print a list with published information!

--- simplesite/bboard/views.py
+++ simplesite/bboard/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Bb
 
 # Create your views here.
 def index(request):
-    return HttpResponse("List with announcements!")
+    list_publish = 'List with publish \r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        list_publish += bb.title + '\r\n'+ bb.content + '\r\n\r\n'
+    return HttpResponse(list_publish, content_type='text/plain; charset=utf-8')
