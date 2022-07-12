32844d5a68144cb51a483496c0881021ad0b884a
Andriyevski <andriyevski@gmail.com>
2022-07-13 00:05:38 +0300

    I create a simple html page and change view to work with her

--- simplesite/bboard/views.py
+++ simplesite/bboard/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Bb
 
 # Create your views here.
 def index(request):
+    template = loader.get_template('index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs':bbs}
+    return HttpResponse(template.render(context, request))
+
+def another(request):
     list_publish = 'List with publish \r\n\r\n\r\n'
     for bb in Bb.objects.order_by('-published'):
         list_publish += bb.title + '\r\n'+ bb.content + '\r\n\r\n'
